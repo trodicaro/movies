7517d8646d1b54e264b9ad69bce81a3ee06fd81f
Rodica Trifan <trodicaro@gmail.com>
2018-04-13 23:53:56 -0500

    Disable showtime if no more seats

--- app/models/screening.rb
+++ app/models/screening.rb
@@ -1,10 +1,16 @@
 class Screening < ApplicationRecord
   belongs_to :theater
   belongs_to :movie
+  has_many :orders
 
   scope :scheduled_today, -> { where(scheduled_at: Date.today.all_day) }
 
-  def sold_out
+  def sold_out?
+    total_seats = self.orders.pluck(:seat_count).reduce(:+)
+
+    return false if !total_seats
+
+    total_seats > self.theater.seating
   end
 
   def scheduled_today?
