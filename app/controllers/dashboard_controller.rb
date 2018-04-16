2b4aa055ddd66c9c242ff7692c17601e969d5d11
Rodica Trifan <trodicaro@gmail.com>
2018-04-15 19:48:03 -0500

    Fix data series in last chart; use https for image links

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,10 @@ class DashboardController < ApplicationController
     @movies = Movie.includes({orders: :customer}).limit(5)
     @movie_sales = Movie.includes(:orders).group("movies.name").sum('orders.total_cost').sort_by{|k,v| v}.reverse.transpose
     @popular_movies = Movie.ordered_by_popularity.limit(20).map { |movie| [movie.name, movie.popularity]}.transpose
-    @daily_sales= @orders.order("date_trunc('day', screenings.scheduled_at) ASC, orders.total_cost ASC").pluck("date_trunc('day', screenings.scheduled_at)", :total_cost).group_by(&:first).map {|day, records| [day.strftime("%a, %m-%d"),records.map(&:last).reduce(:+)]}.transpose
+    @daily_sales= @orders.order("date_trunc('day', screenings.scheduled_at) ASC, orders.total_cost ASC")
+      .pluck("date_trunc('day', screenings.scheduled_at)", :total_cost)
+      .group_by(&:first)
+      .map {|day, records| [day.strftime("%a, %m-%d"), records.map(&:last).reduce(:+)]}
+      .transpose
   end
 end
